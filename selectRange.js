da5fb94626131c63a85a80efbde28747baf2b1be
Razvan Iacob <razvan.iacoob@gmail.com>
2014-09-26 11:45:05 +0300

    Initial commit

---
+++ selectRange.js
@@ -0,0 +1,12 @@
+$.fn.selectRange = function (options) {
+
+    var $settings = $.extend({min: 0, max: 20}, options),
+        $options = '',
+        $val = this.val();
+
+    $select.empty();
+    for(i = $settings.min, i <= $settings.max, i++) {
+        $options += '<oprion value="' + i + '">' + i + '</option>';
+    }
+    return this.html($options).val($val);
+};
