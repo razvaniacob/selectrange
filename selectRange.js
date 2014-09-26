546881008200ccf2b0d02afa2e337a83cbc70498
Razvan Iacob <razvan.iacoob@gmail.com>
2014-09-26 12:32:56 +0300

    Update selectRange.js

--- selectRange.js
+++ selectRange.js
@@ -1,12 +1,16 @@
-$.fn.selectRange = function (options) {
+(function($) {
 
-    var $settings = $.extend({min: 0, max: 20}, options),
-        $options = '',
-        $val = this.val();
-
-    this.empty();
-    for(i = $settings.min; i <= $settings.max; i++) {
-        $options += '<option value="' + i + '"' + (i == $val ? ' selected="selected"' : '') + '>' + i + '</option>';
-    }
-    return this.html($options);
-};
+    $.fn.selectRange = function(options) {
+    
+        var $settings = $.extend({min: 0, max: 20}, options),
+            $options = '',
+            $val = this.val();
+    
+        this.empty();
+        for(i = $settings.min; i <= $settings.max; i++) {
+            $options += '<option value="' + i + '"' + (i == $val ? ' selected="selected"' : '') + '>' + i + '</option>';
+        }
+        return this.html($options);
+    };
+    
+})(jQuery);
